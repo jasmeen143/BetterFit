3a3c5f2acd89e2f9578c30ba4ec10b03478df498
jasi981 <jasi981@vidalibarraquer.net>
2020-06-04 18:39:36 +0200

    little changes

--- app/src/main/java/singh/betterfit/Exercise.java
+++ app/src/main/java/singh/betterfit/Exercise.java
@@ -6,15 +6,15 @@ import java.util.List;
 public class Exercise {
     private String nameMuscle;
     private String nameExercise;
-    private int gif;
+    private String gifLink;
     private static List<Exercise> allExercises = new ArrayList<Exercise>();
 
     public Exercise() {}
 
-    public Exercise(String nameMuscle, String nameExercise, int gif) {
+    public Exercise(String nameMuscle, String nameExercise, String gifLink) {
         this.nameMuscle = nameMuscle;
         this.nameExercise = nameExercise;
-        this.gif = gif;
+        this.gifLink = gifLink;
     }
 
     public String getNameMuscle() {
@@ -33,146 +33,55 @@ public class Exercise {
         this.nameExercise = nameExercise;
     }
 
-    public int getGif() {
-        return gif;
+    public String getGifLink() {
+        return gifLink;
     }
 
-    public void setGif(int gif) {
-        this.gif = gif;
+    public void setGifLink(String gifLink) {
+        this.gifLink = gifLink;
     }
 
     public List<Exercise> addAllExercises(Exercise ex) {
         allExercises.add(ex);
         return allExercises;
     }
-/*
-        names.add("Chest");
-        names.add("Forearms");
-        names.add("Glutes");
-        names.add("Hamstrings");
-        names.add("Lats");
-        names.add("MidBack");
-        names.add("Quadriceps");
-        names.add("Shoulders");
-        names.add("Traps");
-        names.add("Triceps");*/
-    public void createAllExercises(){
+
+    public void createAllExercises() {
+        //ABS STRETCHES
+        noNameMethod("Abs","Crunches","https://musclewiki.org/media/uploads/Crunch-Side-021316.gif");
+        noNameMethod("Abs","Forearm Plank","https://musclewiki.org/media/uploads/Plank-Side-021316.gif");
+        noNameMethod("Abs","Roll-Outs","https://musclewiki.org/media/uploads/AbRoller-Side-021316.gif");
+        noNameMethod("Abs","Laying Leg Raises","https://musclewiki.org/media/uploads/LegRaises-Side-021316.gif");
+        noNameMethod("Abs","Hanging Knee Raises","https://musclewiki.org/media/uploads/bodyweight-male-hanging-knee-raises-side.gif");
+        //BICEPS STRETCHES
+
+        //CHEST STRETCHES
+
+        //GLUTES STRETCHES
+
+        //HAMSTRINGS STRETCHES
+
+        //LATS STRETCHES
+
+        //MIDBACK STRETCHES
+
+        //QUADRICEPS STRETCHES
+
+        //SHOULDERS STRETCHES
+
+        //TRAPS STRETCHES
+
+        //TRICEPS STRETCHES
+
+
+    }
+    public void noNameMethod(String nameMuscle, String nameExercise, String gifLink){
         Exercise exercise = new Exercise();
+        exercise.setNameMuscle(nameMuscle);
+        exercise.setNameExercise(nameExercise);
+        exercise.setGifLink(gifLink);
 
-        /*----------------------------------------------------------------------------*/
-        exercise.setNameMuscle("Abs");
-        exercise.setNameExercise("Crunches");
-        exercise.setGif(R.drawable.abs_crunches);
         addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("Abs");
-        exercise.setNameExercise("Forearm Plank");
-        exercise.setGif(R.drawable.abs_forearmplank);
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("Abs");
-        exercise.setNameExercise("Roll-Outs");
-        exercise.setGif(R.drawable.abs_rollouts);
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("Abs");
-        exercise.setNameExercise("Laying Leg Raises");
-        exercise.setGif(R.drawable.abs_layinglegraises);
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("Abs");
-        exercise.setNameExercise("Hanging Knee Raises");
-        exercise.setGif(R.drawable.abs_hangingkneeraises);
-        addAllExercises(exercise);
-
-        /*----------------------------------------------------------------------------*/
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("Biceps");
-        exercise.setNameExercise("Hammer Curl");
-        exercise.setGif(R.drawable.biceps_hammercurl);
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("Biceps");
-        exercise.setNameExercise("Dumbbell Curl");
-        exercise.setGif(R.drawable.biceps_dumbellcurl);
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("Biceps");
-        exercise.setNameExercise("Barbell Curl");
-        exercise.setGif(R.drawable.biceps_barbellcurl);
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("Biceps");
-        exercise.setNameExercise("Chin-Ups");
-        exercise.setGif(R.drawable.biceps_chinups);
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("Biceps");
-        exercise.setNameExercise("Inverted Row");
-        exercise.setGif(R.drawable.biceps_invertedrow);
-        addAllExercises(exercise);
-
-        /*----------------------------------------------------------------------------*/
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("Calves");
-        exercise.setNameExercise("Standing Calf Raises");
-        exercise.setGif(R.drawable.calves_standingcalfraises);
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("Calves");
-        exercise.setNameExercise("Seated Calf Raises");
-        exercise.setGif(R.drawable.calves_seatedcalfraises);
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("Calves");
-        exercise.setNameExercise("Tip Toe Walking");
-        exercise.setGif(R.drawable.calves_tiptoewalking);
-        addAllExercises(exercise);
-
-        /*----------------------------------------------------------------------------*/
-/*
-        exercise = new Exercise();
-        exercise.setNameMuscle("");
-        exercise.setNameExercise("");
-        exercise.setGif();
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("");
-        exercise.setNameExercise("");
-        exercise.setGif();
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("");
-        exercise.setNameExercise("");
-        exercise.setGif();
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("");
-        exercise.setNameExercise("");
-        exercise.setGif();
-        addAllExercises(exercise);
-
-        exercise = new Exercise();
-        exercise.setNameMuscle("");
-        exercise.setNameExercise("");
-        exercise.setGif();
-        addAllExercises(exercise);*/
-
     }
 
 
--- app/src/main/java/singh/betterfit/Stretches.java
+++ app/src/main/java/singh/betterfit/Stretches.java
@@ -5,16 +5,16 @@ import java.util.List;
 
 public class Stretches {
     private String nameMuscle;
-    private String nameStretch;
-    private int gif;
+    private int idStretch;
+    private String gifLink;
     private static List<Stretches> allStretches = new ArrayList<Stretches>();
 
     public Stretches() {}
 
-    public Stretches(String nameMuscle, String nameStretch, int gif) {
+    public Stretches(String nameMuscle, int idStretch, String gifLink) {
         this.nameMuscle = nameMuscle;
-        this.nameStretch = nameStretch;
-        this.gif = gif;
+        this.idStretch = idStretch;
+        this.gifLink = gifLink;
     }
 
     public String getNameMuscle() {
@@ -25,24 +25,63 @@ public class Stretches {
         this.nameMuscle = nameMuscle;
     }
 
-    public String getNameStretch() {
-        return nameStretch;
+    public int getIdStretch() {
+        return idStretch;
     }
 
-    public void setNameStretch(String nameStretch) {
-        this.nameStretch = nameStretch;
+    public void setIdStretch(int idStretch) {
+        this.idStretch = idStretch;
     }
 
-    public int getGif() {
-        return gif;
+    public String getGifLink() {
+        return gifLink;
     }
 
-    public void setGif(int gif) {
-        this.gif = gif;
+    public void setGifLink(String gifLink) {
+        this.gifLink = gifLink;
     }
 
-    public List<Stretches> addAllExercises(Stretches str) {
+    public List<Stretches> addAllStretches(Stretches str) {
         allStretches.add(str);
         return allStretches;
+    }
+
+    public void createAllStretches() {
+        //ABS STRETCHES
+        noNameMethod("Abs",1,"https://musclewiki.org/media/uploads/male-strech-3-side.gif");
+        noNameMethod("Abs",2,"https://musclewiki.org/media/uploads/male-strech-2-front.gif");
+        noNameMethod("Abs",3,"https://musclewiki.org/media/uploads/Male-strech-1-front.gif");
+        noNameMethod("Abs",4,"https://musclewiki.org/media/uploads/male-strech-4-side.gif");
+
+        //BICEPS STRETCHES
+
+
+        //CHEST STRETCHES
+
+        //GLUTES STRETCHES
+
+        //HAMSTRINGS STRETCHES
+
+        //LATS STRETCHES
+
+        //MIDBACK STRETCHES
+
+        //QUADRICEPS STRETCHES
+
+        //SHOULDERS STRETCHES
+
+        //TRAPS STRETCHES
+
+        //TRICEPS STRETCHES
+
+
+    }
+    public void noNameMethod(String nameMuscle, int idStretch, String gifLink){
+        Stretches stretches = new Stretches();
+        stretches.setNameMuscle(nameMuscle);
+        stretches.setIdStretch(idStretch);
+        stretches.setGifLink(gifLink);
+
+        addAllStretches(stretches);
     }
 }
